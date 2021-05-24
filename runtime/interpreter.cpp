ac871ade476deedfc4168fdd4cda5775a778f9aa
huqng <huqng@outlook.com>
2021-05-24 15:11:01 +0800

    while loop & break

--- runtime/interpreter.cpp
+++ runtime/interpreter.cpp
@@ -1,3 +1,4 @@
+
 #include "../code/opcode.h"
 #include "../object/pInteger.h"
 #include "interpreter.h"
@@ -11,6 +12,8 @@ void Interpreter::run(CodeObject* co) {
     int code_length = co->_bytecodes->length();
 
     _stack = new ArrayList<PObject*>(co->_stacksize);
+    _loop_stack = new ArrayList<LoopBlock*>();
+
     _consts = co->_consts;
     _names = co->_names;
     _vars = new Map<PString*, PObject*>();
@@ -26,30 +29,48 @@ void Interpreter::run(CodeObject* co) {
             op_arg = byte2 << 8 | byte1;
         }
 
-        PInteger *lhs, *rhs;
-        PObject *u, *v, *w, *attr;
+        //PInteger *lhs, *rhs;
+        /* oprands */
+        PObject *u, *v;
         
         switch (op_code) {
-        case BINARY_ADD: // 23
+        case BINARY_ADD: { // 23
+            u = pop();
             v = pop();
-            w = pop();
-            push(w->add(v));
+            push(v->add(u));
             break;
-        case PRINT_ITEM: // 71
+        }
+        case PRINT_ITEM: { // 71
             v = pop();
             v->print();
             break;
-        case PRINT_NEWLINE: // 72
+        }
+        case PRINT_NEWLINE: { // 72
             cout << endl;
             break;
+        }
+        case BREAK_LOOP: { // 80
+            LoopBlock* lb = _loop_stack->pop();
+            while(stack_level() > lb->_level)
+                pop();
+            pc = lb->_target;
+            break;
+        }
         case RETURN_VALUE: // 83
             pop();
             break;
+        case POP_BLOCK: { // 87
+                /* on normal end of loop */
+                LoopBlock* lb = _loop_stack->pop();
+                while(stack_level() > lb->_level)
+                    pop();
+                break;
+            }
         case STORE_NAME: // 90
-            /* add a variable named _names[oparg] 
-             * whose value is on the top of stack
-             * get value by pop
-             * add to vars table
+            /* 
+             * varname = names[op_arg]
+             * value = pop()
+             * vars[varname] = value
              */
             _vars->put((PString*)_names->get(op_arg), pop());
             break;
@@ -57,30 +78,35 @@ void Interpreter::run(CodeObject* co) {
             push(_consts->get(op_arg));
             break;
         case LOAD_NAME: // 101
+            /*
+             * varname = names[op_arg]
+             * value = vars[var_name]
+             * push(value)
+             */
             push(_vars->get((PString*)_names->get(op_arg)));
 
             break;
         case COMPARE_OP: // 107
-            w = pop();
+            u = pop();
             v = pop();
             switch (op_arg) {
             case LESS:
-                push(v->less(w));
+                push(v->less(u));
                 break;
             case LESS_EQUAL:
-                push(v->le(w));
+                push(v->le(u));
                 break;
             case EQUAL:
-                push(v->eq(w));
+                push(v->eq(u));
                 break;
             case NOT_EQUAL:
-                push(v->ne(w));
+                push(v->ne(u));
                 break;
             case GREATER_EQUAL:
-                push(v->ge(w));
+                push(v->ge(u));
                 break;
             case GREATER:
-                push(v->greater(w));
+                push(v->greater(u));
                 break;
             default:
                 break;
@@ -89,13 +115,17 @@ void Interpreter::run(CodeObject* co) {
         case JUMP_FORWARD: // 110
             pc += op_arg;
             break;
+        case JUMP_ABSOLUTE: // 113
+            pc = op_arg;
+            break;
         case POP_JUMP_IF_FALSE: // 114
             v = pop();
             if(v == Universe::PFalse)
                 pc = op_arg;
             break;
-
-
+        case SETUP_LOOP: // 120
+            _loop_stack->add(new LoopBlock(op_code, pc + op_arg, stack_level()));
+            break;
         default:
             cout << "error: unrecognized opcode [" << (int)op_code << "]" << endl;
             exit(-1);
@@ -110,4 +140,8 @@ void Interpreter::push(PObject* p) {
 
 PObject* Interpreter::pop() {
     return _stack->pop();
+}
+
+int Interpreter::stack_level() {
+    return _stack->size();
 }
--- runtime/interpreter.h
+++ runtime/interpreter.h
@@ -6,18 +6,37 @@
 
 class CodeObject;
 
+class LoopBlock {
+public:
+    unsigned char _type;
+    unsigned int _target;
+    int _level;
+    LoopBlock(
+        unsigned char b_type, 
+        unsigned int b_target, 
+        int b_level):
+        _type(b_type),
+        _target(b_target),
+        _level(b_level) {
+
+    }
+};
+
 class Interpreter {
 private:
     /* initialized in run() */
     ArrayList<PObject*>* _stack;
+    ArrayList<LoopBlock*>* _loop_stack;
     ArrayList<PObject*>* _consts;
     ArrayList<PObject*>* _names;
 
     Map<PString*, PObject*>* _vars;
 
-
-    void push(PObject* p);
-    PObject* pop();
+    /* stack op */
+    void        push(PObject* p);
+    PObject*    pop();
+    int         stack_level();
+    
 public:
     void run(CodeObject* co);
 };
