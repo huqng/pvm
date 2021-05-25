c03441b0315c537317bee90a7a28f94175426593
huqng <huqng@outlook.com>
2021-05-25 15:02:13 +0800

    .

--- runtime/interpreter.cpp
+++ runtime/interpreter.cpp
@@ -7,6 +7,34 @@
 #include <iostream>
 using namespace std;
 
+Interpreter::Interpreter() {
+    op = new op_t[256];
+    for(int i = 0; i < 256; i++)
+        op[i] = &Interpreter::unimplemented;
+    
+    op[1]   = &Interpreter::pop_top;
+    op[23]  = &Interpreter::binary_add;
+    op[71]  = &Interpreter::print_item;
+    op[72]  = &Interpreter::print_newline;
+    op[80]  = &Interpreter::break_loop;;
+    op[83]  = &Interpreter::return_value;
+    op[87]  = &Interpreter::pop_block;
+    op[90]  = &Interpreter::store_name;
+    op[100] = &Interpreter::load_const;
+    op[101] = &Interpreter::load_name;
+    op[107] = &Interpreter::compare_op;
+    op[110] = &Interpreter::jump_forward;
+    op[113] = &Interpreter::jump_absolute;
+    op[114] = &Interpreter::pop_jump_if_false;
+    op[120] = &Interpreter::setup_loop;
+    op[131] = &Interpreter::call_function;
+    op[132] = &Interpreter::make_function;
+
+    _frame = nullptr;
+    debug = 0;
+}
+
+
 void Interpreter::run(CodeObject* co) {
     _frame = new FrameObject(co);
     eval_frame();
@@ -43,124 +71,11 @@ void Interpreter::eval_frame() {
             op_arg = _frame->get_op_arg();
         }
 
-        //PInteger *lhs, *rhs;
         /* oprands */
         PObject *u, *v;
         PObject* retv;
-        
-        switch (op_code) {
-        case POP_TOP: // 1
-            pop();
-            break;
-        case BINARY_ADD: { // 23
-            u = pop();
-            v = pop();
-            push(v->add(u));
-            break;
-        }
-        case PRINT_ITEM: { // 71
-            v = pop();
-            v->print();
-            break;
-        }
-        case PRINT_NEWLINE: { // 72
-            cout << endl;
-            break;
-        }
-        case BREAK_LOOP: { // 80
-            LoopBlock* lb = _frame->loop_stack()->pop();
-            while(stack_level() > lb->_level)
-                pop();
-            _frame->set_pc(lb->_target);
-            break;
-        }
-        case RETURN_VALUE: // 83
-            retv = pop();
-            if(_frame->is_first_frame())
-                return;
-            leave_frame(retv);
-            break;
-        case POP_BLOCK: { // 87
-                /* on normal end of loop */
-                LoopBlock* lb = _frame->loop_stack()->pop();
-                while(stack_level() > lb->_level)
-                    pop();
-                break;
-            }
-        case STORE_NAME: // 90
-            /* 
-             * varname = names[op_arg]
-             * value = pop()
-             * vars[varname] = value
-             */
-            _frame->locals()->put(_frame->names()->get(op_arg), pop());
-            break;
-        case LOAD_CONST: // 100
-            push(_frame->consts()->get(op_arg));
-            break;
-        case LOAD_NAME: // 101
-            /*
-             * varname = names[op_arg]
-             * value = vars[var_name]
-             * push(value)
-             */
-            push(_frame->locals()->get(_frame->names()->get(op_arg)));
 
-            break;
-        case COMPARE_OP: // 107
-            u = pop();
-            v = pop();
-            switch (op_arg) {
-            case LESS:
-                push(v->less(u));
-                break;
-            case LESS_EQUAL:
-                push(v->le(u));
-                break;
-            case EQUAL:
-                push(v->eq(u));
-                break;
-            case NOT_EQUAL:
-                push(v->ne(u));
-                break;
-            case GREATER_EQUAL:
-                push(v->ge(u));
-                break;
-            case GREATER:
-                push(v->greater(u));
-                break;
-            default:
-                break;
-            }
-            break;
-        case JUMP_FORWARD: // 110
-            _frame->set_pc(_frame->get_pc() + op_arg);
-            break;
-        case JUMP_ABSOLUTE: // 113
-            _frame->set_pc(op_arg);
-            break;
-        case POP_JUMP_IF_FALSE: // 114
-            v = pop();
-            if(v == Universe::PFalse)
-                _frame->set_pc(op_arg);
-            break;
-        case SETUP_LOOP: // 120
-            _frame->loop_stack()->add(
-                new LoopBlock(op_code, _frame->get_pc() + op_arg, stack_level())
-            );
-            break;
-        case CALL_FUNCTION: // 131
-            build_frame(pop());
-            break;
-        case MAKE_FUNCTION: // 132
-            v = pop();
-            push(new FunctionObject(v));
-            break;
-
-        default:
-            cout << "error: unrecognized opcode [" << (int)op_code << "]" << endl;
-            exit(-1);
-        }
+        (this->*op[op_code])(op_code, op_arg);
     }
 }
 
@@ -173,4 +88,171 @@ void Interpreter::destroy_frame() {
     FrameObject* tmp = _frame;
     _frame = _frame->sender();
     delete tmp;
+}
+
+/* instructions */
+
+void Interpreter::unimplemented(int code, int arg) { 
+    cout << "error: unrecognized opcode [" << code << "]" << endl;
+    exit(-1);
+}
+
+void Interpreter::pop_top(int code, int arg) {
+    pop();
+    if(debug) {
+        cerr << "POP_TOP" << endl;
+    }
+}
+
+void Interpreter::binary_add(int code, int arg) {
+    PObject *u, *v;
+    u = pop();
+    v = pop();
+    push(v->add(u));
+    if(debug) {
+        cerr << "BINARY_ADD" << endl;
+    }
+}
+
+void Interpreter::print_item(int code, int arg) {
+    PObject* u = pop();
+    u->print();
+    if(debug) {
+        cerr << "PRINT_ITEM (omitted)" << endl;
+    }
+}
+
+void Interpreter::print_newline(int code, int arg) {
+    cout << endl;
+    if(debug) {
+        cerr << "PRINT_NEWLINE" << endl;
+    }
+}
+
+void Interpreter::break_loop(int code, int arg) {
+    LoopBlock* lb = _frame->loop_stack()->pop();
+    while(stack_level() > lb->_level)
+        pop();
+    _frame->set_pc(lb->_target);
+    if(debug) {
+        cerr << "BREAK_LOOP" << endl;
+    }
+}
+
+void Interpreter::return_value(int code, int arg) {
+    PObject* retv = pop();
+    if(_frame->is_first_frame())
+        return;
+    leave_frame(retv);
+    if(debug) {
+        cerr << "RETURN_VALUE";
+    }
+}
+
+void Interpreter::pop_block(int code, int arg) {
+    LoopBlock* lb = _frame->loop_stack()->pop();
+    while(stack_level() > lb->_level)
+        pop();
+    if(debug) {
+        cerr << "POP_BLOCK";
+    }
+}
+
+void Interpreter::store_name(int code, int arg) {
+    _frame->locals()->put(_frame->names()->get(arg), pop());
+    if(debug) {
+        cerr << "STORE_NAME" << endl;
+    }
+}
+
+void Interpreter::load_const(int code, int arg) {
+    push(_frame->consts()->get(arg));
+    if(debug) {
+        cerr << "LOAD_CONST" << endl;
+    }
+}
+
+void Interpreter::load_name(int code, int arg) {
+    push(_frame->locals()->get(_frame->names()->get(arg)));
+    if(debug) {
+        cerr << "LOAD_NAME" << endl;
+    }
+}
+
+void Interpreter::compare_op(int code, int arg) {
+    PObject *u, *v;
+    u = pop();
+    v = pop();
+    switch (arg) {
+    case LESS:
+        push(v->less(u));
+        break;
+    case LESS_EQUAL:
+        push(v->le(u));
+        break;
+    case EQUAL:
+        push(v->eq(u));
+        break;
+    case NOT_EQUAL:
+        push(v->ne(u));
+        break;
+    case GREATER_EQUAL:
+        push(v->ge(u));
+        break;
+    case GREATER:
+        push(v->greater(u));
+        break;
+    default:
+        break;
+    }
+    if(debug) {
+        cerr << "COMPARE_OP" << endl;
+    }   
+}
+
+void Interpreter::jump_forward(int code, int arg) {
+    _frame->set_pc(_frame->get_pc() + arg);
+    if(debug) {
+        cerr << "JUMP_FORWARD" << endl;
+    }
+}
+
+void Interpreter::jump_absolute(int code, int arg) {
+    _frame->set_pc(arg);
+    if(debug) {
+        cerr << "JUMP_ABSOLUTE" << endl;
+    }
+}
+
+void Interpreter::pop_jump_if_false(int code, int arg) {
+    PObject* v = pop();
+    if(v == Universe::PFalse)
+        _frame->set_pc(arg);
+    if(debug) {
+        cerr << "POP_JUMP_IF_FALSE" << endl;
+    }
+}
+
+void Interpreter::setup_loop(int code, int arg) {
+    _frame->loop_stack()->add(
+        new LoopBlock(code, _frame->get_pc() + arg, stack_level())
+    );
+    if(debug) {
+        cerr << "SETUP_LOOP" << endl;
+    }
+}
+
+void Interpreter::call_function(int code, int arg) {
+    build_frame(pop());
+    if(debug) {
+        cerr << "CALL_FUNCTION" << endl;
+    }
+}
+
+void Interpreter::make_function(int code, int arg) {
+    PObject* v = pop();
+    push(new FunctionObject(v));
+    if(debug) {
+        cerr << "MAKE_FUNCTION" << endl;
+    }
 }
--- runtime/interpreter.h
+++ runtime/interpreter.h
@@ -6,6 +6,11 @@
 
 class CodeObject;
 
+class LoopBlock;
+class Interpreter;
+
+typedef void(Interpreter::*op_t)(int, int);
+
 class LoopBlock {
 public:
     unsigned char _type;
@@ -25,6 +30,7 @@ public:
 class Interpreter {
 private:
     FrameObject* _frame;
+    bool        debug;
 
     /* stack op */
     void        push(PObject* p);
@@ -35,7 +41,31 @@ private:
     void        eval_frame();
     void        leave_frame(PObject* retv);
     void        destroy_frame();
+
+    /* instructions */
+    op_t*   op;
+
+    void    unimplemented(int, int);     // ?
+    void    pop_top (int, int);          // 1
+    void    binary_add(int, int);        // 23
+    void    print_item(int, int);        // 71
+    void    print_newline(int, int);     // 72
+    void    break_loop(int, int);        // 80
+    void    return_value(int, int);      // 83
+    void    pop_block(int, int);         // 87
+    void    store_name(int, int);        // 90
+    void    load_const(int, int);        // 100
+    void    load_name(int, int);         // 101
+    void    compare_op(int, int);        // 107
+    void    jump_forward(int, int);      // 110
+    void    jump_absolute(int, int);     // 113
+    void    pop_jump_if_false(int, int); // 114
+    void    setup_loop(int, int);        // 120
+    void    call_function(int, int);     // 131
+    void    make_function(int, int);     // 132
+
 public:
+    Interpreter();
     void run(CodeObject* co);
 };
 
